550812e25004e990a5fe46b88213995029874f01
txmazing <txmazing@gmail.com>
2022-05-04 13:40:08 +0200

    Update Page1.js

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,142 +1,79 @@
 /* Bootstrap */
 import {
-	MDBBtn,
+	MDBCheckbox,
+	MDBAccordion,
+	MDBAccordionItem,
 	MDBCol,
 	MDBContainer,
-	MDBDatepicker,
+	MDBRange,
+	MDBRow,
 	MDBModal,
 	MDBModalBody,
 	MDBModalContent,
-	MDBModalDialog,
 	MDBModalHeader,
-	MDBRow,
+	MDBModalDialog,
+	MDBBtn,
 } from "mdb-react-ui-kit";
-/* React */
+
 import { useState } from "react";
 
 /* Code */
 export default function Page1() {
-	const [modal, setModal] = useState(false);
-	const [modal2, setModal2] = useState(false);
-
-	const [datepickerValue, setDatepickerValue] = useState("");
-
+	const [modalOpen, setModalOpen] = useState(false);
 	return (
 		<>
-			<MDBContainer
-				fluid
-				className='bg-primary text-white'
-				style={{ height: "200vh" }}
+			<MDBModal
+				show={modalOpen}
+				tabIndex='-1'
+				backdrop
+				staticBackdrop
+				setShow={setModalOpen}
 			>
-				<MDBContainer style={{ paddingTop: "75px" }}>
-					<MDBRow>
-						<MDBCol className='pt-3'>
-							<MDBRow className='my-4'>
-								<MDBCol>
-									<MDBDatepicker
-										value={datepickerValue}
-										setValue={setDatepickerValue}
-										inputToggle
-									/>
-									<MDBBtn
-										className='rounded-pill fw-bold mt-4'
-										color='secondary'
-										onClick={() => setModal(true)}
-									>
-										Open modal
-									</MDBBtn>
-								</MDBCol>
-							</MDBRow>
-						</MDBCol>
-					</MDBRow>
-				</MDBContainer>
+				<MDBModalDialog centered>
+					<MDBModalContent className='bg-secondary-bright text-white'>
+						<MDBModalHeader className='border-secondary-bright'>
+							<MDBBtn
+								className='btn-close'
+								color='none'
+								onClick={() => {
+									setModalOpen(false);
+								}}
+							></MDBBtn>
+						</MDBModalHeader>
+						<MDBModalBody className='pt-1 p-3 p-sm-5'>
+							<h3>
+								<strong>Test Modal</strong>
+							</h3>
+						</MDBModalBody>
+					</MDBModalContent>
+				</MDBModalDialog>
+			</MDBModal>
+			<MDBContainer fluid className='bg-primary'>
+				<MDBRow>
+					<MDBCol>
+						<h1 className='font-weight-bold py-3 text-white'>Test</h1>
+					</MDBCol>
+				</MDBRow>
+				<MDBRow>
+					<MDBAccordion alwaysOpen>
+						<MDBAccordionItem collapseId={1} headerTitle='Test1'>
+							<MDBCheckbox value='cb1' label='Checkbox 1' id='test1-cb1' />
+							<MDBCheckbox value='cb2' label='Checkbox 2' id='test1-cb2' />
+						</MDBAccordionItem>
+						<MDBAccordionItem collapseId={2} headerTitle='Test2'>
+							<MDBRange value={10} min='1' max='10' label={`1 to 10`} />
+						</MDBAccordionItem>
+						<MDBAccordionItem collapseId={3} headerTitle='Test3'>
+							<MDBCheckbox value='cb1' label='Checkbox 1' id='test3-cb1' />
+							<MDBCheckbox value='cb2' label='Checkbox 2' id='test3-cb2' />
+							<MDBCheckbox value='cb3' label='Checkbox 1' id='test3-cb3' />
+							<MDBCheckbox value='cb4' label='Checkbox 2' id='test3-cb4' />
+							<MDBCheckbox value='cb5' label='Checkbox 1' id='test3-cb5' />
+							<MDBCheckbox value='cb6' label='Checkbox 2' id='test3-cb6' />
+						</MDBAccordionItem>
+					</MDBAccordion>
+				</MDBRow>
 			</MDBContainer>
-
-			<MDBModal
-				show={modal}
-				tabIndex='-1'
-				backdrop={false}
-				staticBackdrop
-				setShow={setModal}
-			>
-				<MDBModalDialog centered>
-					<MDBModalContent className='bg-secondary-bright text-white'>
-						<MDBModalHeader className='border-secondary-bright'>
-							<MDBBtn
-								className='btn-close'
-								color='none'
-								onClick={() => {
-									setModal(false);
-								}}
-							></MDBBtn>
-						</MDBModalHeader>
-						<MDBModalBody>
-							<MDBBtn
-								className='rounded-pill fw-bold mt-4 '
-								color='primary'
-								onClick={() => setModal(false)}
-							>
-								Close
-							</MDBBtn>
-							<MDBBtn
-								className='rounded-pill fw-bold mt-4 '
-								color='primary'
-								onClick={() => {
-									setModal(false);
-									setTimeout(() => {
-										setModal2(true);
-									}, 200);
-								}}
-							>
-								Next Modal
-							</MDBBtn>
-						</MDBModalBody>
-					</MDBModalContent>
-				</MDBModalDialog>
-			</MDBModal>
-
-			<MDBModal
-				show={modal2}
-				tabIndex='-1'
-				backdrop={false}
-				staticBackdrop
-				setShow={setModal2}
-			>
-				<MDBModalDialog centered>
-					<MDBModalContent className='bg-secondary-bright text-white'>
-						<MDBModalHeader className='border-secondary-bright'>
-							<MDBBtn
-								className='btn-close'
-								color='none'
-								onClick={() => {
-									setModal2(false);
-								}}
-							></MDBBtn>
-						</MDBModalHeader>
-						<MDBModalBody>
-							<MDBBtn
-								className='rounded-pill fw-bold mt-4 '
-								color='primary'
-								onClick={() => setModal2(false)}
-							>
-								Close
-							</MDBBtn>
-							<MDBBtn
-								className='rounded-pill fw-bold mt-4 '
-								color='primary'
-								onClick={() => {
-									setModal2(false);
-									setTimeout(() => {
-										setModal(true);
-									}, 200);
-								}}
-							>
-								Prev Modal
-							</MDBBtn>
-						</MDBModalBody>
-					</MDBModalContent>
-				</MDBModalDialog>
-			</MDBModal>
 		</>
 	);
 }
